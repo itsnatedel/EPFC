c2701890e898edd5d742bca2368623270a8096bc
Nathan D <48414055+mvker@users.noreply.github.com>
2019-11-11 19:06:39 +0100

    Les types d'écriture en PHP

---
+++ Scripts Serveur/Types_d'ecriture.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$love = "Cédryc Ruth";
+echo "N°1 Le plus BG des podcasteurs sur Youtube: " . $love . "</br>";
+
+//On peut aussi écrire de cette manière
+echo "N°2 Le plus BG des podcasteurs sur Youtube: $love </br>";
+
+$x = 4;
+$y = 6;
+//Ceci va écrire le résultat de la somme des variables x et y
+echo $x + $y;
+?>
+</body>
+</html>
