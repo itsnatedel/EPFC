664e65bb2dcc1039575c600dcf99270c2f4b886c
Nathan D <48414055+mvker@users.noreply.github.com>
2019-11-11 18:57:53 +0100

    Create Couleurs.php

---
+++ Scripts Serveur/Couleurs.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$color1 = "red"; //Syntaxe: $var = value;
+$color2 = "green";
+echo "My car is " . $color1 . "</br>"; //Syntaxe : "texte" POINT $var POINT "texte";
+echo "My favorite color is " . $color2 . "</br>";
+?>
+</body>
+</html>
