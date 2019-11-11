ea17115f0d28f5c7b5d6804f598bf2874c606758
Nathan D <48414055+mvker@users.noreply.github.com>
2019-11-11 18:55:14 +0100

    Les types de commentaires en PHP

---
+++ Scripts Serveur/Types_de_commentaire.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+#This is a single-line comment
+//This is also a single-line comment
+/* This one 
+    is for 
+    multiple lines
+*/
+$x = 5*5;
+echo "5² =" . $x . "</br>";
+?>
+</body>
+</html>
