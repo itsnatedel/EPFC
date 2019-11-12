7e6d69a693e6b9dd7622a0393f2aa0872afe2a9b
Nathan D <48414055+mvker@users.noreply.github.com>
2019-11-12 18:06:26 +0100

    Fonctions sur les chaînes de caractères en PHP

---
+++ Scripts Serveur/Fonctions_Chaines_Caractères.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html>
+<body>
+<h1>PHP String Functions</h1>
+<a href="https://www.w3schools.com/php/php_ref_string.asp">Liste des fonctions pour les chaînes de caractères</a>
+<?php
+echo <<<EOL
+<h3>strlen() - Return the Length of a String</h3>
+The PHP strlen() function returns the length of a string.</br>
+Cette chaîne de caractere contient 50 caracteres! </br>
+strlen() output: 
+EOL;
+echo strlen("Cette chaîne de caractere contient 50 caracteres!");
+echo "<hr>";
+
+echo <<<EOL
+<h3>str_word_count() - Count Words in a String</h3>
+The PHP str_word_count() function counts the number of words in a string. </br>
+Cette chaîne de caractere contient 7 mots!</br>
+str_word_count() output: 
+EOL;
+echo str_word_count("Cette chaîne de caractere contient 7 mots!");
+echo "<hr>";
+
+echo <<<EOL
+<h3>strrev() - Reverse a String</h3>
+The PHP strrev() function reverses a string.</br>
+Cette chaine de caractere donne "eretcarac ed eniahc etteC" à l'envers!</br>
+strrev() output:
+EOL;
+echo strrev("Cette chaine de caractere");
+echo "<hr>";
+
+echo <<<EOL
+<h3>strpos() - Search For a Text Within a String</h3>
+The PHP strpos() function searches for a specific text within a string.</br>
+If a match is found, the function returns the character position of the first match.</br>
+If no match is found, it will return FALSE.</br>
+Syntax: strpos("text", "textToSearch"); </br> 
+As follow: strpos("Hello world!", "world"); -> Returns 6 as the word "world" begins at the 6th caracter </br>
+The count begins at 0. A space is also counted as a position.</br>
+strpos() output: 
+EOL;
+echo strpos("Hello world!", "world");
+echo "<hr>";
+
+echo <<<EOL
+<h3>str_replace() - Replace Text Within a String</h3>
+The PHP str_replace() function replaces some characters with some other characters in a string. </br>
+Syntax: str_replace("WordBeingReplaced", "NewWord", "Original Text"); </br>
+As in : str_replace("world", "poucave", "Hello world!"); </br>
+EOL;
+echo "Hello world!<br/>";
+echo str_replace("world", "poucave", "Hello world!");
+echo "<hr>";
+?>
+
+</body>
+</html>
