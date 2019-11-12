4771f09ff6befad403648cce2aa3ed90084abd4b
Nathan D <48414055+mvker@users.noreply.github.com>
2019-11-12 18:09:15 +0100

    Fonctions sur les chaînes de caractères en PHP

--- Scripts Serveur/Fonctions_Chaines_Caractères.php
+++ Scripts Serveur/Fonctions_Chaines_Caractères.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<meta charset="UTF-8">
 <html>
 <body>
 <h1>PHP String Functions</h1>
@@ -38,7 +37,7 @@ The PHP strpos() function searches for a specific text within a string.</br>
 If a match is found, the function returns the character position of the first match.</br>
 If no match is found, it will return FALSE.</br>
 Syntax: strpos("text", "textToSearch"); </br> 
-As follow: strpos("Hello world!", "world"); -> Returns 6 as the word "world" begins at the 6th caracter </br>
+As follow: strpos("Hello world!", "world"); -> Returns 6 as the word "world" begins at the 6th character </br>
 The count begins at 0. A space is also counted as a position.</br>
 strpos() output: 
 EOL;
@@ -49,12 +48,11 @@ echo <<<EOL
 <h3>str_replace() - Replace Text Within a String</h3>
 The PHP str_replace() function replaces some characters with some other characters in a string. </br>
 Syntax: str_replace("WordBeingReplaced", "NewWord", "Original Text"); </br>
-As in : str_replace("world", "poucave", "Hello world!"); </br>
+As follow: str_replace("world", "poucave", "Hello world!"); </br>
 EOL;
 echo "Hello world!<br/>";
 echo str_replace("world", "poucave", "Hello world!");
-echo "<hr>";
-?>
 
+?>
 </body>
 </html>
